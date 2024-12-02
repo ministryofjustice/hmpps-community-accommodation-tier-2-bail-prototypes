b2c51e7957f5a34c691702f7fe83a48e2b6add76
Chris Armstrong <christopher.armstrong@disclosurescotland.gov.scot>
2024-12-02 15:54:02 +0000

    Clean array filter for footer items that might not exist on the hosted prototype

--- app/filters.js
+++ app/filters.js
@@ -7,3 +7,8 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 
+addFilter('cleanArray', (array) => {
+	return array.filter(item => {
+		return (item && (item !==""))
+	})
+})
